c77ff566339b0a808ba8b6fae118998319d82a8a
Jakob Buis <jakob@jakobbuis.nl>
2017-04-17 12:20:14 +0200

    implemented behaviour of Quote component

--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -1,10 +1,32 @@
 <template>
-    <h2 class="subtitle"></h2>
+    <h2 class="subtitle">{{ currentQuote }}</h2>
 </template>
 
 <script>
 export default {
     name: 'quote',
+    data() {
+        const quotes = [
+            'People who enjoy meetings should not be in charge of anything.',
+            'Meetings are indispensable when you don\'t want to do anything.',
+            'Has anyone ever said, \'I wish I could go to more meetings today?',
+        ];
+        return {
+            timeOut: 30 * 1000,
+            quotes,
+            currentQuote: quotes[0],
+        };
+    },
+    methods: {
+        updateQuote() {
+            this.currentQuote = this.quotes[Math.floor(Math.random() * this.quotes.length)];
+            setTimeout(this.updateQuote, this.timeOut);
+        },
+    },
+
+    mounted() {
+        setTimeout(this.updateQuote, this.timeOut);
+    },
 };
 </script>
 
