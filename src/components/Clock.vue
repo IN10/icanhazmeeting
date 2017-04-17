d4526cb1b2d3c35f64ac9ac0925141b601c014b7
Jakob Buis <jakob@jakobbuis.nl>
2017-04-17 12:31:43 +0200

    implemented clock component

--- src/components/Clock.vue
+++ src/components/Clock.vue
@@ -1,9 +1,27 @@
 <template>
-  <span class="pull-right">10:00</span>
+  <span class="pull-right">{{ hours }}:{{ minutes }}</span>
 </template>
 
 <script>
 export default {
     name: 'clock',
+    data() {
+        const now = new Date();
+        return {
+            hours: now.getHours(),
+            minutes: now.getMinutes(),
+        };
+    },
+    methods: {
+        updateTime() {
+            const now = new Date();
+            this.hours = now.getHours();
+            this.minutes = now.getMinutes();
+            setTimeout(this.updateTime, 5000);
+        },
+    },
+    mounted() {
+        this.updateTime();
+    },
 };
 </script>
