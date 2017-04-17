e59ddf781abed4d695e5c94f2503452ecfc55d4a
Jakob Buis <jakob@jakobbuis.nl>
2017-04-17 16:11:23 +0200

    brought config.example.js in line with eslint

--- src/config.example.js
+++ src/config.example.js
@@ -1,22 +1,20 @@
-window.config = {
-    oauth: {
-        client_id: '',
-    },
+export default {
+    oauth_client_id: '',
     rooms: [
         [
-            { id: 'maaskamer',     name: 'Maaskamer',      resourceID: '' },
-            { id: 'spreekkamer',   name: 'Spreekkamer',    resourceID: '' },
-            { id: 'podiumkamer',   name: 'Podiumkamer',    resourceID: '' },
+            { id: 'maaskamer', name: 'Maaskamer', resourceID: '' },
+            { id: 'spreekkamer', name: 'Spreekkamer', resourceID: '' },
+            { id: 'podiumkamer', name: 'Podiumkamer', resourceID: '' },
         ],
         [
-            { id: 'standuplinks',  name: 'Standup links',  resourceID: '' },
+            { id: 'standuplinks', name: 'Standup links', resourceID: '' },
             { id: 'standuprechts', name: 'Standup rechts', resourceID: '' },
             { id: 'standupcreate', name: 'Standup Create', resourceID: '' },
         ],
     ],
     quotes: [
-        "People who enjoy meetings should not be in charge of anything.",
-        "Meetings are indispensable when you don't want to do anything.",
-        "Has anyone ever said, 'I wish I could go to more meetings today'?",
-    ]
+        'People who enjoy meetings should not be in charge of anything.',
+        'Meetings are indispensable when you don\'t want to do anything.',
+        'Has anyone ever said, \'I wish I could go to more meetings today\'?',
+    ],
 };
