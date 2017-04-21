3e9e4eb1f92851d3ff6b04fc48b88a6cf5448f39
Jakob Buis <jakob@jakobbuis.nl>
2017-04-21 21:13:58 +0200

    implemented indicator toggling

--- src/components/Room.vue
+++ src/components/Room.vue
@@ -5,30 +5,60 @@
                 <div class="media-content">
                     <div class="content">
                         <p>
-                            <!-- <span class="indicator tag">{{ room.status }}</span> -->
+                            <span class="indicator tag" v-bind:class="indicatorClass">{{ status }}</span>
                             <strong class=name>{{ config.name }}</strong><br>
-                            <small>{{ config.resourceID }}</small>
-                            <pre>{{ freebusy }}</pre>
-                            <!-- <span class="current">
-                                <small class="status">{{ room.current.status }}</small>
-                                <small class="duration is-pulled-right">{{ room.current.duration }}</small>
+                            <span class="current">
+                                <small class="status">{{ current.status }}</small>
+                                <small class="duration is-pulled-right">{{ current.duration }}</small>
                             </span><br>
                             <span class="upcoming">
-                                <small class="status">{{ room.upcoming.status }}</small>
-                                <small class="duration is-pulled-right">{{ room.upcoming.duration }}</small>
-                            </span> -->
+                                <small class="status">{{ upcoming.status }}</small>
+                                <small class="duration is-pulled-right">{{ upcoming.duration }}</small>
+                            </span>
                         </p>
                     </div>
                 </div>
             </article>
         </div>
     </div>
-</template>
+</template>o
 
 <script>
     export default {
         name: 'room',
         props: ['config', 'freebusy'],
+
+        computed: {
+            // The status indicator of a
+            status() {
+                if (this.freebusy.length === 0) {
+                    return 'Vrij';
+                }
+
+                const now = new Date();
+                const start = new Date(this.freebusy[0].start);
+                const end = new Date(this.freebusy[0].end);
+
+                if (now > start && now < end) {
+                    return 'Bezet';
+                }
+                return 'Vrij';
+            },
+
+            indicatorClass() {
+                if (this.status === 'Vrij') {
+                    return 'is-success';
+                }
+                return 'is-danger';
+            },
+
+            current() {
+                return { status: 'leeg', duration: 5 };
+            },
+            upcoming() {
+                return { status: 'leeg', duration: 5 };
+            },
+        },
     };
 </script>
 
